89113bbc7596cc9914ac664d3ea9d548979ca7e2
Himanshu Rathod <50297306+MattarPan33r@users.noreply.github.com>
2023-03-11 18:29:49 +0530

    Add files via upload

---
+++ Assignment2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int checker(int i);
+int main()
+{
+    int i, num, tmp =0;
+    printf("Enter Number to Check:");
+    scanf("%d",&num);
+    for(i=2;i<num/2;i++)
+    {
+        if(num%i==0)
+        {
+            tmp++;
+            break;
+        }
+    }
+    if(tmp==0 && num!=1)
+    {
+        printf("%d is a Prime Number", num);
+    }
+    else
+    {
+        printf("%d is not a Prime Number", num);
+    }
+    return 0;
+}
